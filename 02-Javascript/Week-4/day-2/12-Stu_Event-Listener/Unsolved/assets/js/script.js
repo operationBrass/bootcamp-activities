35344f886a8d317bbf9c846be20b8cb2a4d49ae5
BRASS <mr.brn.lewis@outlook.com>
2021-04-26 13:04:02 +0000

    more homework

--- 02-Javascript/Week-4/day-2/12-Stu_Event-Listener/Unsolved/assets/js/script.js
+++ 02-Javascript/Week-4/day-2/12-Stu_Event-Listener/Unsolved/assets/js/script.js
@@ -3,18 +3,27 @@ var incrementEl = document.querySelector("#increment");
 var decrementEl = document.querySelector("#decrement");
 var countEl = document.querySelector("#count");
 
-function setCounterText() {
-
-console.log(countEl+1)
-  
-}
 
 // TODO: Add event listener to increment button
 
-incrementEl.onclick(setcountertext);
+let count = 0;
+
+incrementEl.addEventListener("click",function (){
+    count++;
+    countEl.textContent = count;
+});
+
+decrementEl.addEventListener("click",function() {
+    if(count > 0)
+    {
+    count--;
+    countEl.textContent = count;
+    }
+});
+
+
 
 
 
 // TODO: Add event listener to decrement button 
 
-decrementEl.ONCLICK(setcountertext)
