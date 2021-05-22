0c3bfb82654ded3ce9e95e7f59f98e9529dd3f5a
BRASS <mr.brn.lewis@outlook.com>
2021-05-22 14:22:28 +0800

    completed 10-02

--- 04-OOP/Week-10/Day-1/02-Stu_Constructors/Unsolved/index.js
+++ 04-OOP/Week-10/Day-1/02-Stu_Constructors/Unsolved/index.js
@@ -2,20 +2,26 @@
 
 function Developer(name,tech)
 {
-console.log(this);
+
 this.name = name;
 this.tech = tech;
-this.intro = () => console.log("Hi ", name, " ", tech);
-console.log(this);
 
 }
 
-const johnTheDev = new Developer("john", "JavaScript");
-johnTheDev.intro();
+
 
 
 // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
 
+Developer.prototype.introduction = function() {
+    console.log(`Hi ${this.name} who has a tech favourite of ${this.tech}`)
+};
+
+
 // TODO: Create a new object using the 'Developer' constructor
 
+const johnTheDev = new Developer("John", "JavaScript");
+
 // TODO: Call the 'introduction()' method on the new object
+
+johnTheDev.introduction();
