b9e98e9a7d6336074ef17f672b5473a44aa4ea30
BRASS <mr.brn.lewis@outlook.com>
2021-05-22 12:00:55 +0800

    finished the 09 - week 1 homework as missed the class...

--- 03-JS-Node/Week-9/Day-0/10-Stu_Template-Literals/Unsolved/index.js
+++ 03-JS-Node/Week-9/Day-0/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,13 @@
 // music should be an object with title, artist, and album properties
 const music = {
   // code here
- 
+  title: "Toybox",
+  artist: "Insane Clown Posse",
+  album: "Riddlebox"
 };
 
 // Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+const songSnippet = `Here we have ${music.title} by the ${music.artist} from the ${music.album} album!!`;
 
 console.log(songSnippet)
 
