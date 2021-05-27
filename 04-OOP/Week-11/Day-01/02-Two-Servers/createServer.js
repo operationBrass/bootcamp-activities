b4ce51a84f7603e9f46b055016734bc51deaffe1
BRASS <mr.brn.lewis@outlook.com>
2021-05-27 23:58:33 +0800

    week11 02 homework done

--- 04-OOP/Week-11/Day-01/02-Two-Servers/createServer.js
+++ 04-OOP/Week-11/Day-01/02-Two-Servers/createServer.js
@@ -2,12 +2,12 @@ const http = require("http");
 
 function createQuote(port, quote)
 {
-    const server = http.createServer((request,repsonse) => {
-        response.write(quote);
-        response.end();
+    const server = http.createServer((request,response) => {
+        response.end(quote);
     });
 
     server.listen(port);
+    console.log("listening on ", port)
 }
 
 module.exports = createQuote;
--- 04-OOP/Week-11/Day-01/02-Two-Servers/index.js
+++ 04-OOP/Week-11/Day-01/02-Two-Servers/index.js
@@ -1,5 +1,5 @@
 const createQuote = require("./createServer");
-
-const servers = require(createServer);
-const PORT = process.env.PORT;
+const PORT = 8080;
+const PORT2 = 8000;
 createQuote(PORT,"It is what it is what it isnt");
+createQuote(PORT2,"Do as i say not as i do");
