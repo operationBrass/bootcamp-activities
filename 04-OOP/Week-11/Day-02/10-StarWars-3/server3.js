01eedf09a8d6b7df34ec2a584f4ceaa941af28a4
BRASS <mr.brn.lewis@outlook.com>
2021-06-01 19:14:15 +0800

    Finished week 11 day 02 h/w

--- 04-OOP/Week-11/Day-02/10-StarWars-3/server3.js
+++ 04-OOP/Week-11/Day-02/10-StarWars-3/server3.js
@@ -45,12 +45,13 @@ app.get('/api/characters', (req, res) => {
 // processes get request and saves the choosen character as a parameter on the request object.
 app.get('/api/characters/:character', (req, res) => {
   // What does this code do?
-  // assigns the text of :character to the params property
+  // assigns the text of :character to the params property wqich appears to be an array of objects. 
   const chosen = req.params.character;
   console.log(req.params);
 
 
   // What does this code do?
+  //loops through the character objects looking for a match to the character saved in the URL and varible chosen
   for (let i = 0; i < characters.length; i++) {
     const currentChar = characters[i];
     if (chosen === currentChar.routeName) {
@@ -59,6 +60,7 @@ app.get('/api/characters/:character', (req, res) => {
   }
 
   // What does this code do?
+  // returns no character found if no match to in the FOR loop above
   return res.send('No character found');
 });
 
--- 04-OOP/Week-11/Day-02/12-StarWars-5/README.md
+++ 04-OOP/Week-11/Day-02/12-StarWars-5/README.md
@@ -8,7 +8,14 @@
 
 * Spend a few moments researching what `express.json` is for and what `req.body` means in the context of Express.
 
+express.json - Returns middleware that only parses JSON and only looks at requests where the Content-Type header matches the type option.
+req.body - Contains key-value pairs of data submitted in the request body. By default, it is undefined, and is populated when you use body-parsing middleware such as body-parser and multer.
+
   * Then research how you can POST data to the Express server.
+express object called app
+app.post()
+app.all()
+app.method()
 
 ### Bonus
 
