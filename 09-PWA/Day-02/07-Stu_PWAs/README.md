36aef9bc1e61f55b509f2fe5ef661227b47ca0e3
BRASS <mr.brn.lewis@outlook.com>
2021-08-08 15:23:29 +0800

    Do PWA homework - class time

--- 09-PWA/Day-02/07-Stu_PWAs/README.md
+++ 09-PWA/Day-02/07-Stu_PWAs/README.md
@@ -1,41 +1,46 @@
-
 # Progressive Web Applications
 
 In this activity, you will install a progressive web application (PWA) using your smart phone. You will also research the definition and production of a PWA. If you are unable to find the icons mentioned in this activity, try them in Chrome on your computer.
 
 ## Instructions
 
-* Follow these instructions to install a PWA for your specific smartphone OS:
+- Follow these instructions to install a PWA for your specific smartphone OS:
 
-* iOs:
+- iOs:
 
-  * 1. Navigate to [https://image-gallery-cache.herokuapp.com/](https://image-gallery-cache.herokuapp.com/) with Safari.
+  - 1. Navigate to [https://image-gallery-cache.herokuapp.com/](https://image-gallery-cache.herokuapp.com/) with Safari.
 
-  * 2. Tap the Share button in Safari.
+  - 2. Tap the Share button in Safari.
 
-  * 3. Tap the icon labeled Add to Home Screen.
+  - 3. Tap the icon labeled Add to Home Screen.
 
-  * 4. Tap Add in the upper-right corner.
+  - 4. Tap Add in the upper-right corner.
 
-  * 5. Name your PWA, then tap Add in the upper-right corner.
+  - 5. Name your PWA, then tap Add in the upper-right corner.
 
-* Android:
+- Android:
 
-  * 1. Navigate to [https://image-gallery-cache.herokuapp.com/](https://image-gallery-cache.herokuapp.com/) with Chrome.
+  - 1. Navigate to [https://image-gallery-cache.herokuapp.com/](https://image-gallery-cache.herokuapp.com/) with Chrome.
 
-  * 2. Tap the menu button in the upper right corner of Chrome.
+  - 2. Tap the menu button in the upper right corner of Chrome.
 
-  * 3. Tap the icon labeled Add to Home Screen.
+  - 3. Tap the icon labeled Add to Home Screen.
 
-  * 4. Name your PWA, then tap Add below the promp.
+  - 4. Name your PWA, then tap Add below the promp.
 
-* Be prepared to answer the following question(s): 
+- Be prepared to answer the following question(s):
 
-    * What is a progressive web application? 
+  - What is a progressive web application?
 
-    * How do we create progressive web applications?
+  allows users to work offline and appears like a native app but is browswer app.
 
+  - How do we create progressive web applications?
+    We need 3 things
+
+  1. manifest.webmanifest file (JSON file)
+  2. Service Worker
+  3. Cache API
 
 ## 🏆 Bonus
 
-* What are examples of popular PWAs?
+- What are examples of popular PWAs?
--- 09-PWA/Day-02/13-Stu_Caching_Fetching_Files/Unsolved/public/service-worker.js
+++ 09-PWA/Day-02/13-Stu_Caching_Fetching_Files/Unsolved/public/service-worker.js
@@ -1,8 +1,3 @@
-
-
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
@@ -57,3 +52,86 @@ const FILES_TO_CACHE = [
   '/assets/images/37.jpg',
   '/assets/images/38.jpg'
 ];
+
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+// install
+self.addEventListener("install", function (evt) {
+  // pre cache image data
+  evt.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+    );
+    
+  // pre cache all static assets
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(evt) {
+  // code to handle requests goes here
+  });
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+
+  self.addEventListener('fetch',function(evt){
+    if( evt.request.url.includes('/api/')){
+      console.log('[Service Worker] Fetch (data)', evt.request.url);
+
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              if(response.status === 200)
+              {
+                cache.put(evt.request.url, response.clone());
+              }
+          return response;
+
+        })
+
+        .catch(err => {
+          return cache.match(evt.request);
+        });
+      })
+      );
+      return;
+    }
+
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        }); 
+
+      })
+    );
+  });
