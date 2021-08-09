8c59944cd0dc5161c9c9eaa04e2a58ba0c9ed0e4
BRASS <mr.brn.lewis@outlook.com>
2021-08-09 22:39:59 +0800

    finish react day-2 12 h/w

--- 10-React/Day-2/12-Stu_State/Unsolved/src/components/Greeting.js
+++ 10-React/Day-2/12-Stu_State/Unsolved/src/components/Greeting.js
@@ -3,22 +3,22 @@ import React, { useState } from 'react';
 
 function Greeting() {
   // TODO: Convert `greeting` to a state variable using the useState hook. Give it a initial value of 'Welcome the following students to class!'
-  let greeting = 'Welcome the following students to class!';
+  let [greeting,setGreeting] = useState('Welcome the following students to class!');
 
   // TODO: Convert `group` to a state variable using the useState hook. Set to an initial value to an array including three students in your class. (ex. ["John", "Grace", "Jared"])
-  let group = ['', '', ''];
+  let [group,setGroup] = useState(['Silas', 'Sebastian', 'Archer']);
 
   return (
     <div className="card text-center">
       <div className="card-header bg-primary text-white">State activity!</div>
       <div className="card-body">
         {/* TODO: Fix the `p` tag below to instead render the `greeting` variable */}
-        <p className="card-text">Message</p>
+        <p className="card-text">{greeting}</p>
         <ul>
           {/* TODO: Fix the list below so that each member of your group is accessed from the `group` array */}
-          <li>Student 1</li>
-          <li>Student 2</li>
-          <li>Student 3</li>
+          <li>{group[0]}</li>
+          <li>{group[1]}</li>
+          <li>{group[2]}</li>
         </ul>
       </div>
     </div>
